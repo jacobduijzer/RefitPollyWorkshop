3c97e8e49f1f9696a1b0ff967e421a046174fc97
Jacob Duijzer <jduijzer@vaa.com>
2019-01-14 08:57:42 +0100

    Fixed unit test issue

--- src/RefitExample.Tests/Services/RemoteApiServiceShould.cs
+++ src/RefitExample.Tests/Services/RemoteApiServiceShould.cs
@@ -2,6 +2,7 @@
 using RefitExample.Interfaces;
 using RefitExample.Models;
 using RefitExample.Services;
+using System;
 using Xunit;
 
 namespace RefitExample.Tests.Services
@@ -11,7 +12,7 @@ namespace RefitExample.Tests.Services
         private readonly IRemoteApiService _remoteApiService;
 
         public RemoteApiServiceShould() =>
-            _remoteApiService = new RemoteApiService(LogLevel.None);
+            _remoteApiService = new RemoteApiService(LogLevel.None, TimeSpan.FromSeconds(30));
 
         [Fact]
         public void Construct() =>
